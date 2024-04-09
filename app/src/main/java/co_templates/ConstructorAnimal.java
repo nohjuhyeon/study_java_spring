b65b73591c1831d60ad6a184f0f0eb3c9c9cf9d3
nohjuhyeon <njh2720@gmail.com>
2024-04-09 05:52:22 +0000

    quest

---
+++ app/src/main/java/co_templates/ConstructorAnimal.java
@@ -0,0 +1,23 @@
+package co_templates;
+
+public class ConstructorAnimal {
+    public int firstNum;
+    public int secondNum;
+    public int result;
+
+    public void setResult(int number){
+        this.result = number;
+    }
+    public int getResult(){
+        return this.result; 
+    }
+    public void animalSound() {
+        System.out.println("The animal makes a sound");
+    }
+    public void byPass(int fisrtNumber, int secondNumber){
+        this.firstNum = fisrtNumber;
+        this.secondNum = secondNumber;
+        this.result = fisrtNumber + secondNumber;
+        return ;
+    }
+}
